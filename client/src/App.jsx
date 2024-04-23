334705e329dcb28f33fb9d5ff66951033e0869f2
Joshua <Joshuahale829@gmail.com>
2024-04-23 13:29:38 -0500

    It builds, but page doesn't load properly, and there's a bunch of errors in the terminal and the console.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 // Import the necessary modules
-import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import React from 'react';
+import './App.css';
+// import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -9,27 +11,27 @@ import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 
 // Create a new Apollo client and pass it to the ApolloProvider
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
 
 // Create the App component that will render the pages
 function App() {
   return (
-    <ApolloProvider client={client}>
+    // <ApolloProvider client={client}>
       <Router>
         <div>
           <Navbar />
-          <Switch>
-            <Route exact path='/' component={SearchBooks} />
-            <Route exact path='/saved' component={SavedBooks} />
-            <Route exact path='/login' component={LoginForm} />
-            <Route exact path='/signup' component={SignupForm} />
-          </Switch>
+          <Routes>
+            <Route exact path="/" element={<SearchBooks />} />
+            <Route exact path="/saved" element={<SavedBooks />} />
+            <Route exact path="/login" element={<LoginForm />} />
+            <Route exact path="/signup" element={<SignupForm />} />
+          </Routes>
         </div>
       </Router>
-    </ApolloProvider>
+    // </ApolloProvider>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,28 +1,48 @@
-const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => {
-    // get the user token from the headers
-    const token = req.headers.authorization || '';
-    // return the token in the context
-    return { token };
-  },
-});
+const startServer = async () => {
+  const app = express();
+  
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  const PORT = process.env.PORT || 3001;
+  
+  app.listen(PORT, () => {
+    console.log(`Server ready at http://localhost:${PORT}`);
+  });
+};
+  
+startServer();
+  
+// const PORT = process.env.PORT || 3001;
+// const db = require('./config/connection');  
+// const app = express();
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: ({ req }) => {
+//     // get the user token from the headers
+//     const token = req.headers.authorization || '';
+//     // return the token in the context
+//     return { token };
+//   },
+// });
 
-server.applyMiddleware({ app });
+// server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-// Connect to MongoDB
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// // Connect to MongoDB
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
