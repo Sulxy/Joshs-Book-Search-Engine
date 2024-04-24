3024d2d33cfc403db9ecce021f93306abb5459ee
Joshua <Joshuahale829@gmail.com>
2024-04-24 13:21:02 -0500

    Updated connection.js for deployment.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// Use the MONGODB_URI environment variable if available, otherwise use a default URI
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks';
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
