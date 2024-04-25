040e2cc3bc8c5590fcca6c55dc403035813d5d8e
Joshua <Joshuahale829@gmail.com>
2024-04-25 09:31:51 -0500

    Updated connection.js file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Use the MONGODB_URI environment variable if available, otherwise use a default URI
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks';
