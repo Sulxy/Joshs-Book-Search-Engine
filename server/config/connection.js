4930d49b963a9efd4a499a4bd575892e694821e9
Joshua <Joshuahale829@gmail.com>
2024-04-25 08:45:00 -0500

    Added .env file to server folder, and updated .gitignore to include the .env file. Updated .env with the MongoDB_URI, and updated connection.js.

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,9 +5,9 @@ const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks';
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 })
-.then(() => console.log('Connected to MongoDB'))
+.then(() => console.log(`Connected to MongoDB: ${mongoose.connection.host}`))
 .catch((err) => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
